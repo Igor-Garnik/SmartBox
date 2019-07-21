75df792befb88db40c166096a3909473c119dcf0
Igor-Garnik <garnik_igor@ukr.net>
2019-07-22 00:25:10 +0300

    added guards for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {LoginComponent} from "./login/login.component";
-import {RegisterComponent} from "./register/register.component";
-import {ProfileComponent} from "./profile/profile.component";
-import {NotFoundComponent} from "./not-found/not-found.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { ProfileEditComponent } from "./profile-edit/profile-edit.component";
+import {AuthGuardService} from "./auth/auth-guard.service";
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'profile/edit', component: ProfileComponent },
+  { path: 'register', component: SignupComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService] },
+  { path: 'profile/edit', component: ProfileEditComponent, canActivate: [AuthGuardService] },
   { path: '**', component: NotFoundComponent }
 ];
 
