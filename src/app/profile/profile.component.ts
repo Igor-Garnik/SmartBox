47bb918e606bc88bad99084f917c0fa60af871f9
Igor-Garnik <garnik_igor@ukr.net>
2019-07-22 00:22:04 +0300

    edit profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {AuthService} from "../auth/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.sass']
+  styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy{
+  form: FormGroup;
+  subscription: Subscription;
+  title = 'Hi Igor Garnik';
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+  ) { }
 
   ngOnInit() {
+    this.initForm();
   }
 
+  ngOnDestroy(): void {
+    if(this.subscription) this.subscription.unsubscribe();
+  }
+
+  initForm(): void {
+    this.form = this.fb.group({
+      firstName: ['Igor'],
+      lastName: ['Garnik'],
+      country: ['Ukraine'],
+      age: ['35']
+    });
+  }
+
+  logOut(): void {
+    this.subscription = this.authService.logout().subscribe();
+  }
 }
